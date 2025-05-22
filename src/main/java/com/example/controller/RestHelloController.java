002008863773b5dec2f0eb8b40632472b5c20ca0
Serdar-Sara <sarsw70735@gmail.com>
2025-05-22 09:40:54 +0300

    update9

--- src/main/java/com/example/controller/RestHelloController.java
+++ src/main/java/com/example/controller/RestHelloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class RestHelloController {//controller
 	@GetMapping
     public String hello() {
-        return "Hello galatasaray";
+        return "Hello besiktas";
     }
 
 }
